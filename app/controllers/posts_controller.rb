9ef912221971646e21a81942f2f791afedaddcd5
Samira Muñoz <smunozpizarro10@gmail.com>
2024-06-04 11:20:29 -0500

    [CHAT-007] - Add rooms

--- app/controllers/posts_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# class PostController
-class PostsController < ApplicationController
-  before_action :authenticate_user!
-
-  def index; end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'posts#index'
+  resources :rooms
+  root 'rooms#index'
   devise_for :users
 end
